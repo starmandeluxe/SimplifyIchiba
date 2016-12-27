42b6c02908c5e8b70c7d9809e3bdcc445546dca0
Alex <starmandeluxe2001@gmail.com>
2016-12-27 14:27:24 +0900

    Added logic in case item number or item number title tags were not on the page

--- fix_rakuten_item_pages.js
+++ fix_rakuten_item_pages.js
@@ -15,19 +15,34 @@
     var itemNumberTitles =  jQuery('.item_number_title');
     var itemNumbers =  jQuery('.item_number');
     var prices =  jQuery('.rakutenLimitedId_cart');
+
     if (prices.length === 0) {
         prices =  jQuery('#rakutenLimitedId_cart');
     }
+
     var baskets = jQuery('[id=rakutenLimitedId_aroundCart]');
     var pagebody = jQuery('#pagebody');
 
-    for (var i = 0; i < baskets.length; i++)
-    {
+    for (var i = 0; i < baskets.length; i++){
         pagebody.before(baskets[i]);
         baskets[i].before(prices[i]);
-        prices[i].before(itemNumbers[i]);
-        itemNumbers[i].before(itemNumberTitles[i]);
-        itemNumberTitles[i].before(itemNames[i]);
+
+        if (itemNumbers.length > 0) {
+
+            prices[i].before(itemNumbers[i]);
+
+            if (itemNumberTitles.length > 0) {
+
+                itemNumbers[i].before(itemNumberTitles[i]);
+                itemNumberTitles[i].before(itemNames[i]);
+            }
+            else {
+                itemNumbers[i].before(itemNames[i]);
+            }
+        }
+        else {
+            prices[i].before(itemNames[i]);
+        }
     }
 
     jQuery('.item_name').find('br').remove();
